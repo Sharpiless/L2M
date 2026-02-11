34521cad7c44e6b1f7f2e22a13b7bbd7bbcba91f
Sharpiless <1691608003@qq.com>
2026-02-11 11:01:30 +0800

    update v2

--- GIM-benchmarks/TEST_L2MPP_official.sh
+++ GIM-benchmarks/TEST_L2MPP_official.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 gpus=$1
 
+python test.py --gpus $gpus --weight l2mpp --version official --test --batch_size 1 --tests  RobotcarSeason --max_samples 2000
+python test.py --gpus $gpus --weight l2mpp --version official --test --batch_size 1 --tests   RobotcarNight
+python test.py --gpus $gpus --weight l2mpp --version official --test --batch_size 1 --tests RobotcarWeather
+
 python test.py --gpus $gpus --weight l2mpp --version official --test --batch_size 1 --tests          GTASfM
 python test.py --gpus $gpus --weight l2mpp --version official --test --batch_size 1 --tests         ICLNUIM
 python test.py --gpus $gpus --weight l2mpp --version official --test --batch_size 1 --tests        MultiFoV
@@ -10,8 +14,4 @@ python test.py --gpus $gpus --weight l2mpp --version official --test --batch_siz
 python test.py --gpus $gpus --weight l2mpp --version official --test --batch_size 1 --tests            GL3D
 python test.py --gpus $gpus --weight l2mpp --version official --test --batch_size 1 --tests           KITTI --img_size 1240
 python test.py --gpus $gpus --weight l2mpp --version official --test --batch_size 1 --tests      BlendedMVS
-python test.py --gpus $gpus --weight l2mpp --version official --test --batch_size 1 --tests  RobotcarSeason --max_samples 2000
-python test.py --gpus $gpus --weight l2mpp --version official --test --batch_size 1 --tests   RobotcarNight
-python test.py --gpus $gpus --weight l2mpp --version official --test --batch_size 1 --tests RobotcarWeather
-
 python analysis.py --dir dump/zeb --wid l2mpp --version official --verbose
--- romatch/models/model_zoo/__init__.py
+++ romatch/models/model_zoo/__init__.py
@@ -3,8 +3,9 @@ import torch
 from .roma_models import roma_model
 
 weight_urls = "https://huggingface.co/datasets/Liangyingping/L2Mpp-checkpoints/resolve/main/l2m%2B%2B_560x560.pth"
+weight_urlsv2 = "https://huggingface.co/datasets/Liangyingping/L2Mpp-checkpoints/resolve/main/l2m%2B%2B_560x560v2.pth.ckpt"
 
-def l2mpp_model(device, weights=None, dinov2_weights=None, coarse_res = 672, upsample_res = 1344, amp_dtype: torch.dtype = torch.float16):
+def l2mpp_model(device, weights=None, dinov2_weights=None, coarse_res = 672, upsample_res = 1344, amp_dtype: torch.dtype = torch.float16, version="v1"):
     if isinstance(coarse_res, int):
         coarse_res = (coarse_res, coarse_res)
     if isinstance(upsample_res, int):    
@@ -17,6 +18,13 @@ def l2mpp_model(device, weights=None, dinov2_weights=None, coarse_res = 672, ups
     assert coarse_res[1] % 14 == 0, "Needs to be multiple of 14 for backbone"
     
     if weights is None:
+        if version == "v1":
+            weight_urls2load = weight_urls
+        elif version == "v2":
+            weight_urls2load = weight_urlsv2
+        else:
+            raise NotImplementedError
+
         print("[loading]:", weight_urls)
         weights = torch.hub.load_state_dict_from_url(weight_urls, map_location=device)
     model = roma_model(resolution=coarse_res, upsample_preds=True,
